3a21cc6d01f606ff0ecfead9ec6c0f3efb436378
nomayen <nomayen.ohin@gmail.com>
2023-12-11 05:11:46 +0600

    server and route created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+// 10
+
+app.get('/',(req, res)=>{
+    res.send('Coffee making server is running')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
